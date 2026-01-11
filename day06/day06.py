3d061e8c0a2964474ce48147ead2d368c8ca475b
codingARa <74619512+codingARa@users.noreply.github.com>
2026-01-11 20:02:51 +0100

    feat(part1): :sparkles: add solution

--- day06/day06.py
+++ day06/day06.py
@@ -11,13 +11,20 @@ LOGFILENAME = "day06.log"
 TEST_RESULT_PART1 = 4277556
 TEST_RESULT_PART2 = None
 # Expected results for the actual puzzle input - received after solving the puzzle
-RESULT_PART1 = None
+RESULT_PART1 = 5227286044585
 RESULT_PART2 = None
 
 
-def parse_input_lines(lines: list[str]):
-    # TODO-ARA
-    pass
+def parse_input_lines(lines: list[str]) -> tuple[list[list[int]], list[str]]:
+    number_lists = []
+    operator_list = []
+    for line in lines:
+        split_line = line.split()
+        try:
+            number_lists.append([int(num) for num in split_line])
+        except ValueError:
+            operator_list = split_line
+    return number_lists, operator_list
 
 
 def get_puzzle_test_input():
@@ -37,9 +44,32 @@ def get_puzzle_input(script_path: Path):
     return parse_input_lines(lines)
 
 
+def get_total_sum(puzzle_input) -> int:
+    total = 0
+    number_lists, operator_list = puzzle_input
+    number_list_count = len(number_lists)
+
+    for index, operator in enumerate(operator_list):
+        match operator:
+            case "*":
+                logging.debug("Multiplication")
+                column_total = 1
+                for n in range(number_list_count):
+                    column_total *= number_lists[n][index]
+                total += column_total
+            case "+":
+                logging.debug("Addition")
+                column_total = 0
+                for n in range(number_list_count):
+                    column_total += number_lists[n][index]
+                total += column_total
+
+    return total
+
+
 def solution_part1(puzzle_input) -> int:
     logging.debug("solution_part1")
-    pass
+    return get_total_sum(puzzle_input)
 
 
 def solution_part2(puzzle_input) -> int:
--- day06/test_day06.py
+++ day06/test_day06.py
@@ -24,7 +24,8 @@ def test_part2():
     assert solution_part2(test_input) == TEST_RESULT_PART2
 
 
-@pytest.mark.skip(reason="Known solution test - skip unless needed")
+# TODO-ARA: insert back before final commit
+# @pytest.mark.skip(reason="Known solution test - skip unless needed")
 def test_part1_with_known_solution():
     puzzle_input = get_puzzle_input(Path(__file__))
     assert solution_part1(puzzle_input) == RESULT_PART1
